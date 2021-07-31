7c036b58293dbd1af9dc552e8ab50e717f007f12
Ivan Lee <leekwunfung817@gmail.com>
2021-07-31 11:01:07 +0800

    src/detector.c Success to Send out the named pipes message (darknet predict annotation) in function (test_detector_named_pipes_service line 2101)

--- src/detector.c
+++ src/detector.c
@@ -1627,106 +1627,88 @@ void test_detector_namedPipes(char* datacfg, char* cfgfile, char* weightfile, ch
     TCHAR chReadBuf[BUFSIZE];
     BOOL fSuccess = FALSE;
     DWORD  cbRead, cbToWrite, cbWritten, dwMode;
-    LPTSTR lpszPipenameRead = TEXT("\\\\.\\pipe\\darknet_image"), lpszPipenameWrite = TEXT("\\\\.\\pipe\\darknet_anotation");
+    LPTSTR lpszPipenameRead = _T("\\\\.\\pipe\\darknet_image"), lpszPipenameWrite = _T("\\\\.\\pipe\\darknet_anotation");
 
     // Try to open a named pipe; wait for it, if necessary. 
-    while (1)
-    {
+    //while (1)
+    //{
         hPipeRead = CreateFile(
             lpszPipenameRead,   // pipe name 
-            GENERIC_READ |  // read and write access 
-            GENERIC_WRITE,
+            GENERIC_READ | GENERIC_WRITE  // read and write access 
+            ,
             0,              // no sharing 
             NULL,           // default security attributes
             OPEN_EXISTING,  // opens existing pipe 
             0,              // default attributes 
             NULL);          // no template file
-         // Break if the pipe handle is valid. 
+        hPipeWrite = CreateNamedPipe(
+            lpszPipenameWrite,   // pipe name 
+            PIPE_ACCESS_DUPLEX,
+            PIPE_TYPE_MESSAGE|PIPE_READMODE_MESSAGE|PIPE_WAIT,              // no sharing 
+            1,
+            65536,
+            65536,
+            0,
+            NULL);          // no template file
+        /*
+
+
+         // Break if the pipe handle is valid.
         if (hPipeRead != INVALID_HANDLE_VALUE)
+        {
+            _tprintf(TEXT("hPipeRead INVALID_HANDLE_VALUE\n"));
             break;
+        }
+        // Break if the pipe handle is valid.
+        if (hPipeWrite != INVALID_HANDLE_VALUE)
+        {
+            _tprintf(TEXT("hPipeWrite INVALID_HANDLE_VALUE\n"));
+            break;
+        }
+        */
+
+        /*
+
         // Exit if an error other than ERROR_PIPE_BUSY occurs. 
-        if (GetLastError() != ERROR_PIPE_BUSY)
+        if (GetLastError() == ERROR_PIPE_BUSY)
         {
             _tprintf(TEXT("Could not open pipe. GLE=%d\n"), GetLastError());
-            return -1;
+            //return -1;
+            continue;
         }
+        // Exit if an error other than ERROR_PIPE_BUSY occurs. 
+        if (GetLastError() == ERROR_PIPE_BUSY)
+        {
+            _tprintf(TEXT("Could not open pipe. GLE=%d\n"), GetLastError());
+            //return -1;
+            continue;
+        }
+
+        */
+        /*
+        * 
         // All pipe instances are busy, so wait for 20 seconds. 
         if (!WaitNamedPipe(lpszPipenameRead, 20000))
         {
-            printf("Could not open pipe: 20 second wait timed out.");
-            return -1;
-        }
-
-        hPipeWrite = CreateFile(
-            lpszPipenameWrite,   // pipe name 
-            GENERIC_READ |  // read and write access 
-            GENERIC_WRITE,
-            0,              // no sharing 
-            NULL,           // default security attributes
-            OPEN_EXISTING,  // opens existing pipe 
-            0,              // default attributes 
-            NULL);          // no template file
-         // Break if the pipe handle is valid. 
-        if (hPipeWrite != INVALID_HANDLE_VALUE)
-            break;
-        // Exit if an error other than ERROR_PIPE_BUSY occurs. 
-        if (GetLastError() != ERROR_PIPE_BUSY)
-        {
-            _tprintf(TEXT("Could not open pipe. GLE=%d\n"), GetLastError());
-            return -1;
+            printf("Could not open pipe: 20 second wait timed out. lpszPipenameRead");
+            //return -1;
+            continue;
         }
         // All pipe instances are busy, so wait for 20 seconds. 
-        if (!WaitNamedPipe(lpszPipenameWrite, 20000))
+        if (!ConnectNamedPipe(lpszPipenameWrite, 20000))
         {
-            printf("Could not open pipe: 20 second wait timed out.");
-            return -1;
+            printf("Could not open pipe: 20 second wait timed out. lpszPipenameWrite");
+            //return -1;
+            continue;
         }
-
-
-    }
-    while (1)
-    {
-        hPipeWrite = CreateFile(
-            lpszPipenameWrite,   // pipe name 
-            GENERIC_READ |  // read and write access 
-            GENERIC_WRITE,
-            0,              // no sharing 
-            NULL,           // default security attributes
-            OPEN_EXISTING,  // opens existing pipe 
-            0,              // default attributes 
-            NULL);          // no template file
-         // Break if the pipe handle is valid. 
-        if (hPipeWrite != INVALID_HANDLE_VALUE)
-            break;
-        // Exit if an error other than ERROR_PIPE_BUSY occurs. 
-        if (GetLastError() != ERROR_PIPE_BUSY)
-        {
-            _tprintf(TEXT("Could not open pipe. GLE=%d\n"), GetLastError());
-            return -1;
-        }
-        // All pipe instances are busy, so wait for 20 seconds. 
-        if (!WaitNamedPipe(lpszPipenameWrite, 20000))
-        {
-            printf("Could not open pipe: 20 second wait timed out.");
-            return -1;
-        }
-
-
-    }
+        */
+        printf("Reader initialize success.\n");
+        printf("Writer initialize success.\n");
+    //}
     // The pipe connected; change to message-read mode. 
     dwMode = PIPE_READMODE_MESSAGE;
     fSuccess = SetNamedPipeHandleState(
         hPipeRead,    // pipe handle 
-        &dwMode,  // new pipe mode 
-        NULL,     // don't set maximum bytes 
-        NULL);    // don't set maximum time 
-    if (!fSuccess)
-    {
-        _tprintf(TEXT("SetNamedPipeHandleState failed. GLE=%d\n"), GetLastError());
-        return -1;
-    }
-    fSuccess = SetNamedPipeHandleState(
-        hPipeWrite,    // pipe handle 
         &dwMode,  // new pipe mode 
         NULL,     // don't set maximum bytes 
         NULL);    // don't set maximum time 
@@ -1740,6 +1722,7 @@ void test_detector_namedPipes(char* datacfg, char* cfgfile, char* weightfile, ch
     // (Read image)
 
 
+    printf("Begin predict.\n");
 
     // ===== ===== ===== ===== ===== Begin predict ===== ===== ===== ===== =====
     // ===== ===== ===== ===== ===== Begin predict ===== ===== ===== ===== =====
@@ -1751,20 +1734,28 @@ void test_detector_namedPipes(char* datacfg, char* cfgfile, char* weightfile, ch
     int names_size = 0;
     char** names = get_labels_custom(name_list, &names_size); //get_labels(name_list);
 
+    printf("load_alphabet.\n");
     image** alphabet = load_alphabet();
+    printf("parse_network_cfg_custom.\n");
     network net = parse_network_cfg_custom(cfgfile, 1, 1); // set batch=1
+    printf("load_weights.\n");
     if (weightfile) {
         load_weights(&net, weightfile);
     }
+    printf("letter_box.\n");
     if (net.letter_box) letter_box = 1;
+    printf("benchmark_layers.\n");
     net.benchmark_layers = benchmark_layers;
+    printf("fuse_conv_batchnorm.\n");
     fuse_conv_batchnorm(net);
+    printf("calculate_binary_weights.\n");
     calculate_binary_weights(net);
     if (net.layers[net.n - 1].classes != names_size) {
         printf("\n Error: in the file %s number of names %d that isn't equal to classes=%d in the file %s \n",
             name_list, names_size, net.layers[net.n - 1].classes, cfgfile);
         if (net.layers[net.n - 1].classes > names_size) getchar();
     }
+    printf("srand.\n");
     srand(2222222);
     char buff[256];
     char* input = buff;
@@ -1772,7 +1763,9 @@ void test_detector_namedPipes(char* datacfg, char* cfgfile, char* weightfile, ch
     int json_image_id = 0;
     int j;
     float nms = .45;    // 0.4F
+    printf("Begin While True Loop.\n");
     while (1) {
+        _tprintf(TEXT(" ===== Loop Begin ===== \n"));
 
         // ===== Named Pipes Begin
         // ===== Named Pipes Begin
@@ -1784,7 +1777,7 @@ void test_detector_namedPipes(char* datacfg, char* cfgfile, char* weightfile, ch
             fSuccess = ReadFile(
                 hPipeRead,    // pipe handle 
                 chReadBuf,    // buffer to receive reply 
-                BUFSIZE * sizeof(TCHAR),  // size of buffer 
+                10000 * 1024, //BUFSIZE * sizeof(TCHAR),  // size of buffer 
                 &cbRead,  // number of bytes read 
                 NULL);    // not overlapped
             if (!fSuccess && GetLastError() != ERROR_MORE_DATA)
@@ -1797,6 +1790,7 @@ void test_detector_namedPipes(char* datacfg, char* cfgfile, char* weightfile, ch
             return -1;
         }
         image im = load_image(chReadBuf, 0, 0, net.c);
+        _tprintf(TEXT("load image success\n"));
         // ===== Named Pipes End
         // ===== Named Pipes End
         // ===== Named Pipes End
@@ -1881,12 +1875,16 @@ void test_detector_namedPipes(char* datacfg, char* cfgfile, char* weightfile, ch
         free_image(im);
         free_image(sized);
 
+        _tprintf(TEXT(" ===== Loop End ===== \n"));
+
+        /*
         if (!dont_show) {
             wait_until_press_key_cv();
             destroy_all_windows_cv();
         }
+        */
 
-        if (filename) break;
+        //if (filename) break;
     }
 
     // named pipes
@@ -1914,6 +1912,241 @@ void test_detector_namedPipes(char* datacfg, char* cfgfile, char* weightfile, ch
     // ===== ===== ===== ===== ===== End predict ===== ===== ===== ===== =====
     // ===== ===== ===== ===== ===== End predict ===== ===== ===== ===== =====
     // ===== ===== ===== ===== ===== End predict ===== ===== ===== ===== =====
+}
+
+void test_detector_named_pipes_service(char* datacfg, char* cfgfile, char* weightfile, char* filename, float thresh,
+    float hier_thresh, int dont_show, int ext_output, int save_labels, char* outfile, int letter_box, int benchmark_layers)
+{
+    printf(" test_detector_named_pipes_service \n");
+    printf(" read_data_cfg \n");
+    list* options = read_data_cfg(datacfg);
+    printf(" option_find_str \n");
+    char* name_list = option_find_str(options, "names", "data/names.list");
+    int names_size = 0;
+    printf(" get_labels_custom \n");
+    char** names = get_labels_custom(name_list, &names_size); //get_labels(name_list);
+
+    printf(" load_alphabet \n");
+    image** alphabet = load_alphabet();
+    printf(" parse_network_cfg_custom \n");
+    network net = parse_network_cfg_custom(cfgfile, 1, 1); // set batch=1
+    printf(" load_weights \n");
+    if (weightfile) {
+        load_weights(&net, weightfile);
+    }
+    printf(" letter_box \n");
+    if (net.letter_box) letter_box = 1;
+    printf(" benchmark_layers \n");
+    net.benchmark_layers = benchmark_layers;
+    printf(" fuse_conv_batchnorm \n");
+    fuse_conv_batchnorm(net);
+    printf(" calculate_binary_weights \n");
+    calculate_binary_weights(net);
+    if (net.layers[net.n - 1].classes != names_size) {
+        printf("\n Error: in the file %s number of names %d that isn't equal to classes=%d in the file %s \n",
+            name_list, names_size, net.layers[net.n - 1].classes, cfgfile);
+        if (net.layers[net.n - 1].classes > names_size) getchar();
+    }
+    printf(" srand \n");
+    srand(2222222);
+    char buff[256];
+    char* input = buff;
+    char* json_buf = NULL;
+    int json_image_id = 0;
+    FILE* json_file = NULL;
+    printf(" outfile \n");
+    if (outfile) {
+        json_file = fopen(outfile, "wb");
+        if (!json_file) {
+            error("fopen failed");
+        }
+        char* tmp = "[\n";
+        fwrite(tmp, sizeof(char), strlen(tmp), json_file);
+    }
+    int j;
+    float nms = .45;    // 0.4F
+    printf(" while (1) \n");
+    while (1) {
+        if (filename) {
+            strncpy(input, filename, 256);
+            if (strlen(input) > 0)
+                if (input[strlen(input) - 1] == 0x0d) input[strlen(input) - 1] = 0;
+        }
+        else {
+            printf("Enter Image Path: ");
+            fflush(stdout);
+            input = fgets(input, 256, stdin);
+            if (!input) break;
+            strtok(input, "\n");
+        }
+
+        // named pipes Begin
+        printf(" named pipes Begin \n");
+        HANDLE hPipeRead, hPipeWrite;
+        LPTSTR lpszWrite = TEXT("Default message from client");
+        TCHAR chReadBuf[BUFSIZE];
+        BOOL fSuccess = FALSE;
+        DWORD  cbRead, cbToWrite, cbWritten, dwMode;
+        LPTSTR lpszPipenameRead = _T("\\\\.\\pipe\\darknet_image");
+
+        printf(" CreateNamedPipe \n");
+        hPipeWrite = CreateNamedPipe(
+            _T("\\\\.\\pipe\\darknet_anotation"),   // pipe name 
+            PIPE_ACCESS_DUPLEX,
+            PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,              // no sharing 
+            1,
+            65536,
+            65536,
+            0,
+            NULL);          // no template file
+
+        printf(" lpszPipenameWrite \n");
+        if (hPipeWrite != INVALID_HANDLE_VALUE) {
+            printf(" Correct create named pipe \n");
+            if (!ConnectNamedPipe(hPipeWrite, NULL)) {
+                printf(" Incorrect connect named pipe \n");
+                break;
+            }
+            else {
+                printf(" Correct connect named pipe \n");
+            }
+        }
+
+        printf(" named pipes End \n");
+        // named pipes End
+
+        //image im;
+        //image sized = load_image_resize(input, net.w, net.h, net.c, &im);
+        printf(" load_image \n");
+        image im = load_image(input, 0, 0, net.c);
+        image sized;
+        if (letter_box) sized = letterbox_image(im, net.w, net.h);
+        else sized = resize_image(im, net.w, net.h);
+
+        layer l = net.layers[net.n - 1];
+        int k;
+        for (k = 0; k < net.n; ++k) {
+            layer lk = net.layers[k];
+            if (lk.type == YOLO || lk.type == GAUSSIAN_YOLO || lk.type == REGION) {
+                l = lk;
+                printf(" Detection layer: %d - type = %d \n", k, l.type);
+            }
+        }
+
+        //box *boxes = calloc(l.w*l.h*l.n, sizeof(box));
+        //float **probs = calloc(l.w*l.h*l.n, sizeof(float*));
+        //for(j = 0; j < l.w*l.h*l.n; ++j) probs[j] = (float*)xcalloc(l.classes, sizeof(float));
+
+        float* X = sized.data;
+
+        //time= what_time_is_it_now();
+        double time = get_time_point();
+        printf(" network_predict \n");
+        network_predict(net, X);
+        //network_predict_image(&net, im); letterbox = 1;
+        printf("%s: Predicted in %lf milli-seconds.\n", input, ((double)get_time_point() - time) / 1000);
+        //printf("%s: Predicted in %f seconds.\n", input, (what_time_is_it_now()-time));
+
+        int nboxes = 0;
+        detection* dets = get_network_boxes(&net, im.w, im.h, thresh, hier_thresh, 0, 1, &nboxes, letter_box);
+        if (nms) {
+            if (l.nms_kind == DEFAULT_NMS) do_nms_sort(dets, nboxes, l.classes, nms);
+            else diounms_sort(dets, nboxes, l.classes, nms, l.nms_kind, l.beta_nms);
+        }
+        draw_detections_v3(im, dets, nboxes, thresh, names, alphabet, l.classes, ext_output);
+        save_image(im, "predictions");
+        if (!dont_show) {
+            show_image(im, "predictions");
+        }
+
+        printf(" detection_to_json \n");
+        if (json_file) {
+            if (json_buf) {
+                char* tmp = ", \n";
+                fwrite(tmp, sizeof(char), strlen(tmp), json_file);
+            }
+            ++json_image_id;
+            json_buf = detection_to_json(dets, nboxes, l.classes, names, json_image_id, input);
+
+            fwrite(json_buf, sizeof(char), strlen(json_buf), json_file);
+            free(json_buf);
+        }
+
+        printf(" save_labels \n");
+        // pseudo labeling concept - fast.ai
+        if (save_labels)
+        {
+            char labelpath[4096];
+            replace_image_to_label(input, labelpath);
+
+            FILE* fw = fopen(labelpath, "wb");
+            int i;
+            for (i = 0; i < nboxes; ++i) {
+                char buff[1024];
+                int class_id = -1;
+                float prob = 0;
+                for (j = 0; j < l.classes; ++j) {
+                    if (dets[i].prob[j] > thresh && dets[i].prob[j] > prob) {
+                        prob = dets[i].prob[j];
+                        class_id = j;
+                    }
+                }
+                if (class_id >= 0) {
+                    sprintf(buff, "%d %2.4f %2.4f %2.4f %2.4f\n", class_id, dets[i].bbox.x, dets[i].bbox.y, dets[i].bbox.w, dets[i].bbox.h);
+                    // named pipes begin
+                    //while (1) {
+
+                    printf(" Send the annotation to named pipes [%s] \n", buff);
+                    cbToWrite = (lstrlen(buff) + 1) * sizeof(TCHAR);
+                    fSuccess = WriteFile(
+                        hPipeWrite,                  // pipe handle 
+                        buff,             // message 
+                        cbToWrite,              // message length 
+                        &cbWritten,             // bytes written 
+                        NULL);                  // not overlapped
+
+                    //}
+                    // named pipes end
+                    fwrite(buff, sizeof(char), strlen(buff), fw);
+                }
+            }
+            fclose(fw);
+        }
+
+        free_detections(dets, nboxes);
+        free_image(im);
+        free_image(sized);
+
+        if (!dont_show) {
+            wait_until_press_key_cv();
+            destroy_all_windows_cv();
+        }
+
+        if (filename) break;
+    }
+
+    if (json_file) {
+        char* tmp = "\n]";
+        fwrite(tmp, sizeof(char), strlen(tmp), json_file);
+        fclose(json_file);
+    }
+
+    // free memory
+    free_ptrs((void**)names, net.layers[net.n - 1].classes);
+    free_list_contents_kvp(options);
+    free_list(options);
+
+    int i;
+    const int nsize = 8;
+    for (j = 0; j < nsize; ++j) {
+        for (i = 32; i < 127; ++i) {
+            free_image(alphabet[j][i]);
+        }
+        free(alphabet[j]);
+    }
+    free(alphabet);
+
+    free_network(net);
 }
 
 
@@ -2331,6 +2564,7 @@ void run_detector(int argc, char **argv)
     char *filename = (argc > 6) ? argv[6] : 0;
     if (0 == strcmp(argv[2], "test")) test_detector(datacfg, cfg, weights, filename, thresh, hier_thresh, dont_show, ext_output, save_labels, outfile, letter_box, benchmark_layers);
     else if (0 == strcmp(argv[2], "testByNamedPipes")) test_detector_namedPipes(datacfg, cfg, weights, filename, thresh, hier_thresh, dont_show, ext_output, save_labels, outfile, letter_box, benchmark_layers);
+    else if (0 == strcmp(argv[2], "test_detector_named_pipes_service")) test_detector_named_pipes_service(datacfg, cfg, weights, filename, thresh, hier_thresh, dont_show, ext_output, save_labels, outfile, letter_box, benchmark_layers);
     else if (0 == strcmp(argv[2], "train")) train_detector(datacfg, cfg, weights, gpus, ngpus, clear, dont_show, calc_map, mjpeg_port, show_imgs, benchmark_layers, chart_path);
     else if (0 == strcmp(argv[2], "valid")) validate_detector(datacfg, cfg, weights, outfile);
     else if (0 == strcmp(argv[2], "recall")) validate_detector_recall(datacfg, cfg, weights);
